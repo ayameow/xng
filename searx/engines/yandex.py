884dc7743fc433112495a7c976dad6798288b6ec
ayameow <ayameow@users.noreply.github.com>
2026-09-18 02:43:44 +0000

    Create buildx.yml

--- searx/engines/yandex.py
+++ searx/engines/yandex.py
@@ -1,10 +1,13 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 """Yandex (Web, images)"""
 
+from datetime import datetime, timedelta
 import typing as t
 from json import JSONDecodeError, loads
 from urllib.parse import urlencode
 from lxml import html
+
+from searx.search.processors.abstract import TimeRangeType
 from searx.exceptions import SearxEngineCaptchaException, SearxEngineResponseException
 from searx.result_types import EngineResults
 from searx.utils import humanize_bytes, eval_xpath, eval_xpath_list, extract_text, html_to_text
@@ -26,8 +29,10 @@ about = {
 
 # Engine configuration
 categories = []
+time_range_support = True
 paging = True
 enable_http3 = True
+
 search_type = ""
 
 # Search URL
@@ -52,6 +57,7 @@ results_xpath = '//li[contains(@class, "serp-item")]'
 url_xpath = './/a[@class="b-serp-item__title-link"]/@href'
 title_xpath = './/h3[@class="b-serp-item__title"]/a[@class="b-serp-item__title-link"]/span'
 content_xpath = './/div[@class="b-serp-item__content"]//div[@class="b-serp-item__text"]'
+date_xpath = './/span[@data-mtime]/@data-mtime'
 
 
 def catch_bad_response(resp: "SXNG_Response") -> None:
@@ -59,34 +65,57 @@ def catch_bad_response(resp: "SXNG_Response") -> None:
         raise SearxEngineCaptchaException()
 
 
+time_range_to_days_map: dict[TimeRangeType, int] = {
+    "day": 1,
+    "week": 7,
+    "month": 30,
+    "year": 365,
+}
+
+
+def _time_range_to_start_date(time_range: TimeRangeType) -> datetime:
+    diff = timedelta(days=time_range_to_days_map[time_range])
+    return datetime.now() - diff
+
+
 def request(query: str, params: "OnlineParams") -> None:
-    query_params_web = {
-        "tmpl_version": "releases",
+    args: dict[str, t.Any] = {
         "text": query,
-        "web": "1",
-        "frame": "1",
-        "searchid": "3131712",
     }
+    if params['pageno'] > 1:
+        args["p"] = params["pageno"] - 1
 
-    lang = params["language"].split("-")[0]  # type: ignore
-    if lang in yandex_supported_langs:
-        query_params_web["lang"] = lang
+    if search_type == 'web':
+        lang = params["searxng_locale"].split("-")[0]
+        if lang in yandex_supported_langs:
+            args["lang"] = lang
 
-    query_params_images = {
-        "text": query,
-        "uinfo": "sw-1920-sh-1080-ww-1125-wh-999",
-    }
+        if time_range := params["time_range"]:
+            start_date = _time_range_to_start_date(time_range)
+            end_date = datetime.now()
+            args.update(
+                {
+                    "within": 777,  # must be set for time range search, meaning unclear
+                    "from_day": start_date.day,
+                    "from_month": start_date.month,
+                    "from_year": start_date.year,
+                    "to_day": end_date.day,
+                    "to_month": end_date.month,
+                    "to_year": end_date.year,
+                }
+            )
 
-    if params["pageno"] > 1:
-        query_params_web["p"] = params["pageno"] - 1  # type: ignore
-        query_params_images["p"] = params["pageno"] - 1  # type: ignore
-
-    params["cookies"] = {"cookie": "yp=1716337604.sp.family%3A0#1685406411.szm.1:1920x1080:1920x999"}
-
-    if search_type == "web":
-        params["url"] = f"{base_url_web}?{urlencode(query_params_web)}"
-    elif search_type == "images":
-        params["url"] = f"{base_url_images}?{urlencode(query_params_images)}"
+        args.update(
+            {
+                "tmpl_version": "releases",
+                "web": "1",
+                "frame": "1",
+                "searchid": "3131712",
+            }
+        )
+        params['url'] = f"{base_url_web}?{urlencode(args)}"
+    elif search_type == 'images':
+        params['url'] = f"{base_url_images}?{urlencode(args)}"
 
 
 def _parse_json_results(dom: html.HtmlElement) -> dict[str, t.Any]:
@@ -106,7 +135,7 @@ def _parse_json_results(dom: html.HtmlElement) -> dict[str, t.Any]:
 def response(resp: "SXNG_Response") -> EngineResults:
     catch_bad_response(resp)
     results = EngineResults()
-    dom = html.fromstring(resp.text)
+    dom = resp.html()
 
     match search_type:
         case "web":
@@ -114,7 +143,16 @@ def response(resp: "SXNG_Response") -> EngineResults:
                 url = extract_text(eval_xpath(result, url_xpath))
                 title = extract_text(eval_xpath(result, title_xpath))
                 content = extract_text(eval_xpath(result, content_xpath))
-                results.add(results.types.MainResult(url=url, title=str(title), content=str(content)))
+
+                # published date is only shown if time range search is used
+                publishedDate = None
+                if publishedDateMillis := extract_text(eval_xpath(result, date_xpath)):
+                    publishedDate = datetime.utcfromtimestamp(int(publishedDateMillis))
+                results.add(
+                    results.types.MainResult(
+                        url=url, title=str(title), content=str(content), publishedDate=publishedDate
+                    )
+                )
         case "images":
             json_resp = _parse_json_results(dom)
 
